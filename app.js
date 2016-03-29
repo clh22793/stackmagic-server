24e06c6dff90bcae1640afaeb14a9ac9cc9f699e
Chris Hickey <chickey@Chriss-MacBook-Air.local>
2016-03-29 10:51:07 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,305 @@
+var express = require('express');
+//var MongoClient = require('mongodb').MongoClient;
+var Promise = require('bluebird');
+var MongoClient = Promise.promisifyAll(require('mongodb')).MongoClient;
+var bodyParser = require('body-parser');
+var crypto = require('crypto');
+var uuid = require('node-uuid');
+
+
+var app = express();
+
+// parse application/x-www-form-urlencoded 
+//app.use(bodyParser.urlencoded({ extended: false }));
+ 
+// parse application/json 
+app.use(bodyParser.json());
+
+var state = {
+  db: null
+};
+
+// exceptions
+var PayloadException = function(message){
+	this.message = message;
+	this.code = 1000;
+};
+
+var HeaderException = function(message, code){
+	this.message = message;
+	this.code = (code) ? code : 1001;
+};
+
+var ObjectException = function(message, code){
+	this.message = message;
+	this.code = (code) ? code : 1001;
+};
+
+var stackmagic = {
+	'validate_api_key': function(content){
+		return new Promise(function(resolve) {
+			var x_api_key = content.request.headers['x-api-key'];
+
+			var cursor =state.db.collection('versions').find({"id":content.version_id, "client_id":x_api_key, "active":true}).toArray(function(err, docs){
+				content.versions = docs;
+				content.client_id = x_api_key;
+				resolve(content);
+			});
+		});
+	},
+
+	'insert_api_object': function(content){
+		return new Promise(function(resolve) {
+			var cursor = state.db.collection('api_objects').insertOne(content.api_object, function(err, result){
+				content.insert_result = result;
+				resolve(content);
+			});
+		});
+	},
+
+	'get_user_by_api': function(content){
+		return new Promise(function(resolve) {
+			var cursor =state.db.collection('api_objects').find({"body.username": content.request.body.username, "api_id": content.api_id, "active": true}).toArray(function(err, docs){
+				content.api_object_users = docs;
+				resolve(content);
+			});
+		});
+	},
+
+	'get_swagger': function(content){
+		// get swagger for this version
+
+		return new Promise(function(resolve) {
+			var cursor =state.db.collection('swaggers').find({"api_id":content.api_id, "version_id":content.version_id, "active":true}).toArray(function(err, results){
+				//console.log('result');
+				//console.log(result);
+				console.log(err);
+				content.swagger = results;
+				resolve(content);
+			});
+		});
+	},
+
+	/*'client_auth': function(content){
+		console.log('client_auth');
+		console.log(content);
+
+		return new Promise(function(resolve) {
+			console.log('client_auth from stackmagic');
+			console.log(content.request.headers);
+			var x_api_key = content.request.headers['x-api-key'];
+
+			var cursor =state.db.collection('versions').find({"id":content.version_id, "client_id":x_api_key, "active":true}).toArray()
+				.then(function(doc){
+					console.log('finding client...');
+					console.log(doc);
+
+					if(doc.length > 0){
+						content.client_id = doc[0].client_id;
+						resolve(content);
+					}else{
+						console.log('throw invalid api-key exception');
+						throw new HeaderException("invalid x-api-key");
+					}
+
+				});
+		});
+	},*/
+
+	'build_api_object': function(content){
+		return new Promise(function(resolve) {
+			var spec = content.spec;
+			//var spec = content.swagger;
+			var request = content.request;
+
+			var parameters = spec.paths['/users'][request.method.toLowerCase()].parameters[0];
+			var definitions = spec.definitions;
+
+			// check for valid method
+			if(!spec.paths['/users'][request.method.toLowerCase()]){
+				console.log('method NOT ALLOWED');
+				return 'method not allowed';
+			}
+
+			// check for required body params
+			if(parameters.required == true && parameters.in == 'body'){
+				var body = {};
+
+				// get reference definition
+				var schema = parameters.schema;
+				var schema_parts = schema['$ref'].split('#/definitions/');
+				var required_params = definitions[schema_parts[1]].required;
+				var properties = definitions[schema_parts[1]].properties;
+
+				// build payload
+				for(var key in properties){
+					if(properties[key].readOnly == true){
+						continue;
+					}else if(request.body[key]){
+						if(schema_parts[1] == 'User' && key.toLowerCase() == 'password'){
+							body[key] = crypto.createHash('sha1').update(request.body[key]+request.body['username']).digest("hex")
+						}else{
+							body[key] = request.body[key];
+						}
+					}
+				}
+
+				// confirm that payload has all required params
+				for(var i=0; i < required_params.length; i++){
+					if(!body[required_params[i]]){
+						throw new PayloadException("missing parameter: "+required_params[i]);
+					}
+				}
+			}
+
+			if(request.method.toLowerCase() == 'post'){
+				body._created = new Date().toISOString();
+				body._type = schema_parts[1];
+				body._id = crypto.createHash('sha1').update(body._created+body._type+uuid.v4()).digest("hex");
+			}
+
+			content.api_object = {"body":body, "type":schema_parts[1], "api_id":content.api_id, "version_id":content.version_id,
+								  "client_id":content.client_id, "active":true};
+			resolve(content);
+		});
+	}
+};
+
+MongoClient.connect('mongodb://devtest:devtest@aws-us-east-1-portal.14.dblayer.com:10871,aws-us-east-1-portal.13.dblayer.com:10856/dev-saasdoc?authMechanism=SCRAM-SHA-1', function(err, db) {
+  if (err) {
+    throw err;
+  }else{
+    state.db = db;
+  }
+});
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+var get_path_details_from_spec = function(spec, request){
+	console.log('get_auth_from_spec');
+	var path_parts = request.route.path.split('/api/:api_id/:version_id');
+
+	var spec_path = spec.paths[path_parts[1].toLowerCase()];
+	return spec_path;
+};
+
+var get_auth_from_spec = function(spec_path, request){
+
+	var security = spec_path[request.method.toLowerCase()].security;
+
+	console.log(security);
+
+	if(security[0].client_auth){
+		return 'client_auth';
+	}else{
+		return 'user_auth';
+	}
+};
+
+var get_swagger = function(api_id, version_id, request){
+  	// get swagger for this version
+
+	return new Promise(function(resolve) {
+		var cursor =state.db.collection('swaggers').find({"api_id":api_id, "version_id":version_id, "active":true}).toArray()
+			.then(function(doc){
+				var spec = JSON.parse(doc[0].content);
+				console.log(spec);
+				var spec_path = get_path_details_from_spec(spec, request);
+
+				// get the auth from swagger spec
+
+				var auth = get_auth_from_spec(spec_path, request);
+				console.log(auth);
+
+				resolve({"auth":auth, "spec":spec, "request":request, "api_id":api_id, "version_id":version_id, "x-api-key":request.headers['x-api-key']});
+			});
+  	});
+};
+
+var validate_swagger = function(){
+	return new Promise(function(resolve) {
+		var cursor =state.db.collection('swaggers').find({"api_id":api_id, "version_id":version_id, "active":true}).toArray()
+			.then(function(doc){
+				var spec = JSON.parse(doc[0].content);
+				console.log(spec);
+				var spec_path = get_path_details_from_spec(spec, request);
+
+				// get the auth from swagger spec
+
+				var auth = get_auth_from_spec(spec_path, request);
+				console.log(auth);
+
+				resolve({"auth":auth, "spec":spec, "request":request, "api_id":api_id, "version_id":version_id, "x-api-key":request.headers['x-api-key']});
+			});
+  	});
+};
+
+
+app.post('/api/:api_id/:version_id/users', function (request, response) {
+  var api_id = request.params.api_id;
+  var version_id = request.params.version_id;
+
+	var content = {};
+	content.api_id = api_id;
+	content.version_id = version_id;
+	content.request = request;
+
+	stackmagic.get_swagger(content)
+		.then(function(content){
+			return new Promise(function(resolve) {
+				// validate swagger
+
+				if(!content.swagger || content.swagger.length == 0){
+					throw new HeaderException('no available definition for version: '+content.version_id);
+				}else{
+					content.swagger = content.swagger[0];
+					resolve(content);
+				}
+			});
+
+		})
+		.then(stackmagic.validate_api_key)
+		.then(function(content){
+			return new Promise(function(resolve) {
+				if(!content.versions || content.versions.length == 0){
+					throw new HeaderException('invalid x-api-key');
+				}else{
+					content.spec = JSON.parse(content.swagger.content);
+					resolve(content);
+				}
+			});
+		})
+		.then(stackmagic.get_user_by_api)
+		.then(function(content){
+			return new Promise(function(resolve) {
+				// err if user already exists
+
+				if(content.api_object_users.length > 0){
+					throw new ObjectException('user already exists');
+				}else{
+					resolve(content);
+				}
+			});
+		})
+		.then(stackmagic.build_api_object)
+		.then(stackmagic.insert_api_object)
+		.then(function(content){
+			return new Promise(function(resolve) {
+				resolve(content);
+			});
+		})
+		.then(function(content){
+			response.send(content.api_object);
+		})
+		.catch(function(err){
+			console.trace();
+			console.log(err);
+			response.send({"error_code":err.code, "error_message":err.message});
+		});
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
